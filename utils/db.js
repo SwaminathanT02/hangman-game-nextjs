f87c6abe45ca4f0f41b72dcea7846bd094387140
SwaminathanT02 <swaminathant02@gmail.com>
2023-12-26 14:43:53 +0530

    Fixed MongoDB issues

--- utils/db.js
+++ utils/db.js
@@ -4,10 +4,7 @@ const Connect = async () => {
     if (mongoose.connections[0].readyState) return;
 
     try {
-        await mongoose.connect(process.env.MONGO_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        await mongoose.connect(process.env.MONGO_URL);
         console.log("Successfully Connected to Mongo Database");
     } catch (err) {
         throw new Error("Error Connecting To Mongoose!");
